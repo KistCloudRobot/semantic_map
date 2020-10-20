fcfcaa5a5e3455bd31b87af2163e4c31dc719611
yoonseon-oh <yoonseon_oh@brown.edu>
2020-10-20 17:53:33 +0900

    init

---
+++ main.py
@@ -0,0 +1,39 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import numpy as np
+import scipy.spatial as spatial
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    # settings
+    minx = 0; maxx = 10; sizex = maxx-minx;
+    miny = 0; maxy = 10; sizey = maxy-miny;
+    num_node = 50  # graph the number of nodes
+    min_dist = 0.5  # minimum distance between two nodes
+
+    #initialize
+    node_pnt = np.ndarray((num_node, 2))
+
+    node_pnt[0:1, :] = np.multiply(np.random.rand(1, 2),[sizex, sizey]) + [minx, miny]
+    idx = 1
+    for ii in range(1, num_node*2):
+        pnt = np.multiply(np.random.rand(1, 2),[sizex, sizey]) + [minx, miny]
+        y = spatial.distance.cdist(node_pnt[0:idx, :], pnt)
+        if min(y) > min_dist:
+            node_pnt[idx:idx+1, :] = pnt
+            idx = idx+1
+
+        if idx > num_node:
+            print("end")
+            break
+
+    print(idx)
+
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ venv/lib/python3.7/site-packages/scipy/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM SCIPY SETUP.PY
+short_version = '1.5.3'
+version = '1.5.3'
+full_version = '1.5.3'
+git_revision = 'd286f8525c16b2cd4e179dea2c77b6b09622aff9'
+release = True
+
+if not release:
+    version = full_version
