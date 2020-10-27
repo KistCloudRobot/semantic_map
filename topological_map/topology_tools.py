95e423eb53120d3734efd54ce725b8a497d2c35c
yoonseon-oh <41294373+yoonseon-oh@users.noreply.github.com>
2020-10-27 15:40:45 +0900

    issue in scoring

--- topological_map/topology_tools.py
+++ topological_map/topology_tools.py
@@ -22,16 +22,17 @@ class TopoMap():
         self.num_room = len(self.room_classes)
 
         # scoring function
-        self.CONST_DIST = 1
-        self.CONST_CONF = 0.5
+        self.CONST_DIST = 10
+        self.CONST_CONF = 2
 
         # initialize
         self.prm = self.makePRM() # make PRM
         self.compute_dist() # compute distance between nodes
         self.obj_node= {} #{vertex: object}
         self.obj_class ={}
-        self.node_room = np.zeros((self.num_node,self.num_room))# probability
+        self.prob_node_room = np.zeros((self.num_node,self.num_room))# probability
         self.score_room_obj={} # (room, obj): score
+
 
 
     def makePRM(self): # probabilistic roadmap
@@ -123,52 +124,87 @@ class TopoMap():
             # if a new object class is detected, compute room-object score
             if obj_class[key] not in self.obj_class.values():
                 for ii in range(0, self.num_room):
-                    self.score_room_obj[(self.room_classes[ii], obj_class[key])]\
-                        = CNetGetScore(self.room_classes[ii], obj_class[key], 'in')
+                    self.score_room_obj[(self.room_classes[ii], obj_class[key])]= 1/(1+np.exp(-CNetGetScore(obj_class[key], self.room_classes[ii], 'in')/self.CONST_CONF))
 
             self.obj_node[key] = np.argmin((self.nodes[:, 0]-val[0])**2 + (self.nodes[:, 1]-val[1])**2)
             self.obj_class[key] = obj_class[key]
+        self.num_obj = len(self.obj_node)
 
-    def draw_map(self):
+
+    def draw_map(self, draw_room):
         print("Drawing")
         # draw topological map
-        plt.scatter(self.nodes[:, 0], self.nodes[:, 1])
         for ii in range(0, self.num_node):
             for jj in range(ii, self.num_node):
                 if self.edges_mat[ii, jj]>0:
                     plt.plot([self.nodes[ii, 0],self.nodes[jj, 0]],
-                    [self.nodes[ii, 1], self.nodes[jj, 1]], color="b", mfc="r",marker="o")
+                    [self.nodes[ii, 1], self.nodes[jj, 1]], color="b")
+        # Draw nodes
+        plt_node = []
+        for ii in range(0, self.num_node):
+            plt_node.append(plt.plot(self.nodes[ii, 0], self.nodes[ii, 1], c="r", mfc="r", marker="o"))
+
         # draw object
         for key, val in self.obj_node.items():
             plt.text(self.nodes[val,0]+0.1,self.nodes[val,1]+0.1,key)
+
+        # test - distance
+        #for ii in range(0, self.num_node):
+        #    for jj in range(0, self.num_node):
+        #        linecolor = (np.exp(-self.dist_mat[jj, ii]/self.CONST_DIST), 0, 0)
+        #        plt.setp(plt_node[jj], mfc=linecolor, c=linecolor)
+        #    plt.title(str(ii))
+        #    plt.pause(5)
+
+        if draw_room:
+            for ii in range(0, self.num_room):
+                for jj in range(0,self.num_node):
+                    linecolor = (self.prob_node_room[jj,ii],0,0)
+                    plt.setp(plt_node[jj], mfc=linecolor, c=linecolor)
+                plt.title(self.room_classes[ii])
+                plt.pause(10)
+
+
         plt.show()
 
     # TODO: here and fcn_scoring
     def update_nodeclass(self): # update probability
-        for ii in range(0, self.num_node):
-            # probability initialize
-            prob = np.zeros(1, self.num_room)
-            # filtering object near nodes
-
+        prob = np.ones((self.num_node, self.num_room)) # initialize
+        # compute
+        for ii in range(0, self.num_obj):
+            prob_room = np.zeros((self.num_node, self.num_room))
             for jj in range(0, self.num_room):
+                print(self.room_classes[jj]+" "+self.obj_class[ii])
+                prob_room[:, jj:jj+1] = self.fcn_scoring(self.dist_mat[:,self.obj_node[ii]:self.obj_node[ii]+1],
+                                                     self.score_room_obj[(self.room_classes[jj],self.obj_class[ii])])
+            prob = np.multiply(prob, prob_room)
+
+            for kk in range(0, self.num_node):
+                plt.plot(prob_room[kk, :])
+
+        # normalize
+        for ii in range(0, self.num_node):
+            plt.plot(self.prob[ii,:])
+        self.prob_node_room = np.copy(np.divide(prob, np.tile(np.sum(prob, axis=1, keepdims=True), (1, self.num_room))))
+        print("end:update node_class")
 
     def fcn_scoring(self, dist, conf): # distance between vertices, confidence level
         # dist, conf: integer or numpy array
-        score = np.divide(np.exp(-dist/self.CONST_DIST),(1+np.exp(-conf/self.CONST_CONF)))
+        score = np.exp(-dist/self.CONST_DIST)*conf
         return score
-
-
-            print("")
 
 
 
 if __name__ == '__main__':
     topomap = TopoMap()
 
-    obj_pose = {1:[1,1], 2:[1,1.5],3:[0.5,0.5], 4:[3,3],5:[4,3.5]}
-    obj_class = {1:"fridge", 2:"table", 3:"cup", 4:"sofa", 5: "coffee_table"} # ID -class
+    obj_pose = {0:[1,1], 1:[1,1.5], 2:[0.5,0.5], 3:[3,3], 4:[4,3.5], 5:[8,8]}
+    obj_class = {0:"fridge", 1:"table", 2:"food", 3:"sofa", 4: "coffee_table", 5: "balls"} # ID -class
     topomap.update_obj(obj_pose, obj_class)
-    topomap.draw_map()
+    topomap.update_nodeclass()
+    topomap.draw_map(draw_room = True)
+
+
 
     print("end?")
     #fig=spatial.voronoi_plot_2d(vor)
